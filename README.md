e0a458c50ce42556489e78007ad86d7a306141a3
Muhammad <germihanovmohmad@gmail.com>
2024-04-10 00:56:16 +0300

    Updated README and fixed the way i was installing plugins with vimplug

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-###  My current nvim config
+# My current nvim config
+
+## Prerequisites
+- [Neovim](https://neovim.io/): The text editor itself.
+- [Vim-plug](https://github.com/junegunn/vim-plug): A plugin manager.
+<br><br>
+
+## Getting started
+To use this configuration, follow these steps:
+<br><br>
+
+
+### 1.  Clone the repo
+Clone this repository into your Neovim configuration directory (typically `~/.config/nvim/` or `~/.nvim/`)
+```
+git clone https://github.com/gmohmad/Nvim-config.git
+```
+### 2.  Install the plugins
+Open Neovim and run the following command to install all the plugins listed in the configuration:
+```
+:PlugInstall
+```
+
+#### That's it! you can now give this repo a nice little star ...
--- lua/plug.lua
+++ lua/plug.lua
@@ -1,29 +1,31 @@
 -- Plugins with vim-plug
-vim.cmd([[
-	call plug#begin()
+local vim = vim
+local Plug = vim.fn['plug#']
 
 
-	Plug 'vim-airline/vim-airline'
-	Plug 'okuuva/auto-save.nvim'
-	Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
-
-	Plug 'hrsh7th/nvim-cmp'
-	Plug 'hrsh7th/cmp-nvim-lsp'
-	Plug 'hrsh7th/cmp-buffer'
-	Plug 'hrsh7th/cmp-path'
-
-	Plug 'neovim/nvim-lspconfig'
-
-	Plug 'numToStr/Comment.nvim'
-	Plug 'windwp/nvim-autopairs'
-	Plug 'kylechui/nvim-surround'
-
-	Plug 'akinsho/toggleterm.nvim', {'tag' : '*'}
-	Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.6' }
-	Plug 'nvim-lua/plenary.nvim'
-
-	Plug 'catppuccin/nvim', { 'as': 'catppuccin' }
+vim.call('plug#begin')
 
 
-	call plug#end()
-]])
+Plug('vim-airline/vim-airline')
+Plug ('okuuva/auto-save.nvim')
+Plug ('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
+
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+
+Plug('neovim/nvim-lspconfig')
+
+Plug('numToStr/Comment.nvim')
+Plug('windwp/nvim-autopairs')
+Plug('kylechui/nvim-surround')
+
+Plug('akinsho/toggleterm.nvim', { ['tag'] = '*' })
+Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.6' })
+Plug('nvim-lua/plenary.nvim')
+
+Plug('catppuccin/nvim', { ['as'] = 'catppuccin' })
+
+
+vim.call('plug#end')
