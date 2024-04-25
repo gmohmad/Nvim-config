25b21b863fb795c62018b6c7b1a6708f4ed0cadf
Muhammad <germihanovmohmad@gmail.com>
2024-04-25 18:56:10 +0300

    Update lua/remap.lua

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,13 +3,13 @@ function move_lines_down_visual()
 	local current_line = vim.api.nvim_win_get_cursor(0)[1]
 	local last_line = vim.api.nvim_buf_line_count(0)
 
-	local selected_line_count = vim.fn.line("'>") - vim.fn.line("'<")
+	local selected_lines_count = vim.fn.line("'>") - vim.fn.line("'<")
 
 	-- When we hit the bottom we just reselect the lines
-	if current_line + selected_line_count < last_line then
+	if current_line + selected_lines_count < last_line then
 		vim.api.nvim_command("'<, '>m '>+1")
 		vim.api.nvim_command("normal! gv=gv")
-	elseif current_line + selected_line_count >= last_line then
+	else
 		vim.api.nvim_command("normal! gv")
 	end
 end
@@ -23,11 +23,10 @@ function move_lines_up_visual()
 	if current_line > 1 then
 		vim.api.nvim_command("'<, '>m '<-2")
 		vim.api.nvim_command("normal! gv=gv")
-	elseif current_line >= 1 then 
+	else
 		vim.api.nvim_command("normal! gv")
 	end
 end
-
 
 -- Set key mappings to call the functions above
 vim.api.nvim_set_keymap('v', 'J', [[:lua move_lines_down_visual()<cr>]], { noremap = true, silent = true })
